346efcadec88cf426cbba3ea9e8ad66f2f7740fd
Natalie Perpepaj <perpepajn25@gmail.com>
2017-08-08 16:23:07 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,35 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  hash = style_oo_language(languages).merge(style_functional_language(languages)) {|key,v1,v2| v1}
+  hash.map do |language,data|
+    if oo_languages(languages).include?(language) && functional_languages(languages).include?(language)
+      data[:style] = [:oo,:functional]
+      {language => data}
+    elsif oo_languages(languages).include?(language)
+      data[:style] = [:oo]
+      {language => data}
+    elsif functional_languages(languages).include?(language)
+      data[:style] = [:functional]
+      {language => data}
+    end
+  end
+  return hash
+end
+
+
+def style_oo_language(languages)
+  languages[:oo]
+end
+
+def style_functional_language(languages)
+  languages[:functional]
+end
+
+def oo_languages(languages)
+  languages[:oo].keys
+end
+
+def functional_languages(languages)
+  languages[:functional].keys
 end
